ab9b7f5d115b97ed238a1965313c36b3a065c803
wbham <wbham97@naver.com>
2024-01-18 21:54:30 +0900

    [Feat] - NaverMap 문서 리뷰
    - 본 프로젝트에서는 MapFragment대신 MapView를 사용. 권장되는 방식은 MapFragment
    - 지도는 별다른 처리없이 바로 띄울 수 있지만 지도에서 뭔갈 하려면 지도 객체를 얻어와야함. 이를 Activity의 onCreate에서 getMapAsync를 통해 비동기적으로 지도 객체를 얻어오고 클래스 상단에 Callback을 등록
    - 지도 객체가 성공적으로 불러와져서 생성이 되면 onMapReady가 실행. 이후에 naverMap API를 사용할 수 있음
    - Naver Map에서는 마커와 정보창, Shape 등을 자유롭게 커스텀하여 이용할 수 있음

--- app/src/main/java/org/shop/searchrestaurant/MainActivity.kt
+++ app/src/main/java/org/shop/searchrestaurant/MainActivity.kt
@@ -1,17 +1,73 @@
 package org.shop.searchrestaurant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import org.shop.searchrestaurant.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+    private var isMapInit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+        isMapInit = true
+
+        val cameraUpdate =
+            CameraUpdate.scrollTo(LatLng(37.3666102, 126.783881)).animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
     }
 }
